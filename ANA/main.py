e2da59cc94676f1e72905597c32d16c5ae91bf11
ngoldfarb <nagoldfarb@wpi.edu>
2018-02-10 13:36:24 -0500

    working A*

--- ANA/main.py
+++ ANA/main.py
@@ -1,6 +1,7 @@
 import sys
 from PIL import Image
 import copy
+import Queue
 
 '''
 These variables are determined at runtime and should not be changed or mutated by you
@@ -31,7 +32,6 @@ def search(map):
     provided map.
     :param map: A '1-concept' PIL PixelAccess object to be searched. (basically a 2d boolean array)
     """
-
     # O is unoccupied (white); 1 is occupied (black)
     print "pixel value at start point ", map[start[0], start[1]]
     print "pixel value at end point ", map[end[0], end[1]]
@@ -45,6 +45,7 @@ def search(map):
     # put your frontier nodes into this dictionary (so visualize_search can draw them in light gray)
     frontier.update({(6,2):True, (6,3):True, (6,4):True, (6,5):True, (6,6):True, (6,7):True})
 
+
     '''
     YOUR WORK HERE.
     
@@ -52,7 +53,93 @@ def search(map):
         -Gunnar (the TA)-
     '''
 
+    came_from = improved_solution(map)
+    path.extend(reconstruct_path(came_from))
     visualize_search("out.png") # see what your search has wrought (and maybe save your results)
+
+
+
+
+
+def reconstruct_path(came_from):
+    """
+    reconstructs path
+    :param came_from:
+    :return:
+    """
+    current = end
+    my_path = []
+    # this_step = PoseStamped()
+    step_index = 0
+    while current != start:
+        my_path.append(current)
+        current = came_from[current]
+        # print my_path.poses[-1]
+    my_path.append(start)
+    my_path.reverse()  # optional
+    return my_path
+
+
+def improved_solution(map):
+
+    frontierQ = Queue.PriorityQueue()
+    frontierQ.put(start)
+    came_from = {}
+    cost_so_far = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
+    count = 0
+
+    while not frontierQ.empty():
+
+        current = frontierQ.get()
+        count += 1
+
+        if current == end:
+            break
+
+        for next in get_neighbours(map, current):
+            new_cost = cost_so_far[current] + 1
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + heuristic(end, next)
+                frontierQ.put(next, priority)
+                came_from[next] = current
+    print count
+    return came_from
+
+
+def update_eplison():
+    pass
+
+def get_neighbours(map, point):
+    """
+    return a list of neighbours to a point
+    :param point: point to find the neighbours off
+    :return: list of points
+    """
+    loc_x = point[0]
+    loc_y = point[1]
+    width, height = im.size
+    print "width",width
+    print "height",height
+    neighbors_in = [(loc_x - 1, loc_y), (loc_x, loc_y + 1), (loc_x + 1, loc_y), (loc_x, loc_y - 1)]
+    neighbors_out = []
+
+    for option in neighbors_in:
+        if (option[0] >=0 and option[0] < width) and (option[1] >=0 and option[1] < height):
+            if not map[option[0],option[1]]:
+                neighbors_out.append(option)
+
+    return neighbors_out
+
+
+def heuristic(a, b):
+    (x1, y1) = a
+    (x2, y2) = b
+    return abs(x1 - x2) + abs(y1 - y2)
+
+
 
 def visualize_search(save_file="do_not_save.png"):
     """
@@ -114,4 +201,5 @@ if __name__ == "__main__":
 
     # Perform search on given image
     im = Image.open(difficulty)
+
     search(im.load())
