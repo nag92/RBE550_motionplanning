ebe182a1b51f4607a5c40003b91065db1c52e29a
ngoldfarb <nagoldfarb@wpi.edu>
2018-04-08 21:48:51 -0400

    static fields working

--- FinalProject/Main3.py
+++ FinalProject/Main3.py
@@ -13,7 +13,7 @@ import FinalProject.Game.spacewar_helper as sw_helper
 full_path = "/home/nathaniel/git/RBE550_motionplanning/FinalProject/keyboard_DMP/"
 replusor = PF.Repulisive_Function(900000000000, 2 * sw_helper.RADIUS + 30)
 attactor = PF.Attractive_Function(0, 10)
-DMP_force = PF.DMP_Potential_Function(120,60/np.pi)
+DMP_force = PF.DMP_Potential_Function(150,100/np.pi)
 
 
 def get_DMP(player,goal):
@@ -102,7 +102,7 @@ def run(game, cursor):
     total_force = np.array([[0], [0]])
     while len(game.get_goals()) > 0:
 
-        goal = get_goal(game.get_player(), game.get_goals())
+        goal = get_goal(game.get_player(), game.get_goals() )
 
         dmp_file = get_DMP(cursor.state,goal.rect.center)
         runner_y = Mod_DMP_runner(dmp_file + "_y.xml", np.asscalar(cursor.state[1]) , goal.rect.centery  )
@@ -110,32 +110,37 @@ def run(game, cursor):
         err_x = 0
         err_y = 0
         tau = dist(cursor.state,goal.rect.center)/math.sqrt( 200**2 + 200**2)
-
+        F = np.array([[0],[0]])
+        print "-----------------------------"
         for _ in np.arange(0, (tau / dt) + 1):
 
-            (x_t, xd_t, xdd_t) = runner_x.step(tau, dt, error=err_x,externail_force=total_force[0])
-            (y_t, yd_t, ydd_t) = runner_y.step(tau, dt, error=err_y,externail_force=total_force[1])
+            (x_t, xd_t, xdd_t) = runner_x.step(tau, dt,error=err_x, externail_force=F[0])
+            (y_t, yd_t, ydd_t) = runner_y.step(tau, dt,error=err_y, externail_force=F[1])
 
             up = 20 * (np.array([[x_t],   [y_t], [0]]) - cursor.state[0:3])
             uv = 20 * (np.array([[xd_t], [yd_t], [0]]) - cursor.state[3:])
 
             F = np.array([[xdd_t], [ydd_t], [0]]) - up - uv
+            print "F",F
+            F[0] = np.asscalar(F[0][0])
+            F[1] = np.asscalar(F[1][0])
             cursor.move(F)
-            err_x = np.asscalar(0.1 * abs(cursor.state[0] - x_t))[0]
-            err_y = np.asscalar(0.1 * abs(cursor.state[1] - y_t))[0]
+            err_x = 0.1 * abs(cursor.state[0] - x_t)[0]
+            err_y = 0.1 * abs(cursor.state[1] - y_t)[0]
+            print "err", err_x
             F_r = get_obs_force(cursor,game)
             F_a = get_goal_force(cursor,goal)
 
-            F =  get_force(cursor,game,goal)
+            F = get_force(cursor,game,goal)
             print cursor.state
-            total_force = F_r + F_a
+            total_force = F_r #+ F_a
             X.append(cursor.state[0])
             Y.append(cursor.state[1])
             game.move_player(cursor.state[0], cursor.state[1])
             game.update()
 
 
-    time_step = np.arange(0, (1 / .001) + 1)
+    time_step = np.arange(0, (1 / 0.001) + 1)
     # plt.plot(X, Y)
     plt.plot(time_step,Y)
     # plt.plot(time_step, F_x)
@@ -161,7 +166,7 @@ def move_8way():
 
 
 if __name__ == "__main__":
-    game = FinalProject.Game.SpaceWar.SpaceWar((600,300),5,5)
+    game = FinalProject.Game.SpaceWar.SpaceWar((600,300),5,15)
     cursor = Cursor.Cursor(1, 0.01)
     update = True
 
